d1091f93c7fab6d5c7338f78eb88ca3fabfdb3df
ip-10-117-182-168 <bedlow564@gmail.com>
2024-10-24 14:09:10 +0000

    Completed html parse exercise

--- go_homework/go.mod
+++ go_homework/go.mod
@@ -1,3 +1,5 @@
 module go_homework
 
 go 1.23.1
+
+require golang.org/x/net v0.30.0
--- go_homework/main.go
+++ go_homework/main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+
+	"bytes"
+
+	"golang.org/x/net/html"
 )
 
 var raw = `
@@ -16,6 +22,41 @@ var raw = `
 </html>
 `
 
+func visit(n *html.Node, words, pics *int) { // if same type in a row just add type at the end of last var
+	//if its an element node, what tag does it have?
+
+	if n.Type == html.TextNode {
+		*words += len(strings.Fields(n.Data)) //dont forget to de-reference pointer
+
+	} else if n.Type == html.ElementNode && n.Data == "img" {
+		*pics++
+		fmt.Println("Attributes:", n.Attr[0].Key + ": " + n.Attr[0].Val)
+	}
+
+	for child := n.FirstChild; child != nil; child = child.NextSibling {
+		visit(child, words, pics)
+	}
+}
+
+func countWordsAndImages(doc *html.Node) (int, int) {
+	var words, pics int
+	visit(doc, &words, &pics) //dont forget to pass address to pointer parameter
+
+	return words, pics
+}
+
 func main() {
+
+	doc, err := html.Parse(bytes.NewReader([]byte(raw)))
 	fmt.Println(raw)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "parse failed: %s\n", err)
+		os.Exit(-1)
+	}
+
+	words, pics := countWordsAndImages(doc)
+
+	fmt.Printf("%d words and %d images\n", words, pics)
+
 }
