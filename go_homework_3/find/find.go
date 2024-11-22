ceacec4f1975e1f6ac2ff9303acb8ba9c12bbc58
bedlow564 <bmedlow564@gmail.com>
2024-11-21 19:26:03 -0500

    Added homework 3 lesson

--- go_homework_3/find/find.go
+++ go_homework_3/find/find.go
@@ -1,1 +1,91 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// {"month": "4",
+//	"day": "20",
+//	"year": "2009",
+//	"num:" 571,
+//	...
+//	"transcript:" "[[Some is in bed, ... long int.",
+//	"img":	"https://imgs.xkcd.com/comics/cant_sleep.png",
+//	"title": "Cant sleep",
+//}
+
+type xkcd struct {
+	Num        int    `json: "num"`
+	Day        string `json:"day"`
+	Month      string `json:"month"`
+	Year       string `json:"year"`
+	Title      string `json:"title"`
+	Transcript string `json:"transcript"`
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "no file given")
+		os.Exit(-1)
+	}
+
+	fn := os.Args[1] //file name
+
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "no search term")
+		os.Exit(-1)
+	}
+
+	var (
+		items []xkcd //slice of comics json objs
+		terms []string
+		input io.ReadCloser //reader for file
+		cnt   int
+		err   error
+	)
+
+	if input, err = os.Open(fn); err != nil {
+		fmt.Fprintf(os.Stderr, "bad file: %s\n", err)
+		os.Exit(-1)
+	}
+
+	//decode file
+	err = json.NewDecoder(input).Decode(&items) //decode json and put result into items pointer (points to slice of json structs)
+
+	if input, err = os.Open(fn); err != nil {
+		fmt.Fprintf(os.Stderr, "problem decoding json: %s\n", err)
+		os.Exit(-1)
+	}
+
+	fmt.Fprintf(os.Stderr, "read %d comics\n", len(items)) //prints how many comics properly loaded
+
+	//get search terms
+	for _, search_terms := range os.Args[2:] {
+		terms = append(terms, strings.ToLower(search_terms)) //convert search terms to lower case
+	}
+
+	//search
+outer:
+	for _, item := range items {
+		title := strings.ToLower(item.Title) //convert title and transcript of json to lower case
+		transcript := strings.ToLower(item.Transcript)
+
+		for _, term := range terms {
+			//check if json fields contain search term
+			if !strings.Contains(title, term) || !strings.Contains(transcript, term) {
+				continue outer //break out of loop and continue "outer" loop
+			}
+
+		}
+
+		fmt.Printf("https://xkcd.com/%d/ %s/%s/%s/ %q\n", item.Num, item.Month, item.Day, item.Year, item.Title)
+		cnt++
+	}
+
+	fmt.Fprintf(os.Stderr, "Found %d comics\n", cnt)
+
+}
--- go_homework_3/load/xkcd-load.go
+++ go_homework_3/load/xkcd-load.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 )
+
+func getOne(i int) []byte {
+	url := fmt.Sprintf("https://xkcd.com/%d/info.0.json", i) //returns string with format specifier
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cant read: %s\n", err)
+		os.Exit(-1)
+	}
+
+	defer resp.Body.Close() //make sure web socket closes after function executes
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "skipping %d: got %d\n", i, resp.StatusCode)
+		return nil
+	}
+
+	body, err := io.ReadAll(resp.Body) //read body contents into a byte slice
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "inavlid body: %s\n", err)
+		os.Exit(-1)
+	}
+
+	return body //return byte slice
+
+}
 
 func main() {
 	// bundle variables in a var section if we have more than one
@@ -26,4 +55,31 @@ func main() {
 
 		defer output.Close() //close output after main function executes
 	}
+
+	fmt.Fprint(output, "[")        //print bracket to file to make it an array of json objects
+	defer fmt.Fprintf(output, "]") //defer closed bracket until json is fully downloaded
+
+	for i := 1; fails < 2; i++ {
+		if data = getOne(i); data == nil { //check if website returns with a non 200 OK Status code
+			fails++ //stop if we get two 404s in a row
+			continue
+		}
+
+		if cnt > 0 { //print comma after first object has been written and after every object
+			fmt.Fprint(output, ",") //write comma to output file
+		}
+
+		_, err = io.Copy(output, bytes.NewBuffer(data)) //copy json bytes to output file
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "stopped: %s\n", err)
+			os.Exit(-1)
+		}
+
+		fails = 0 //reset fails
+		cnt++     //increment count
+
+	}
+
+	fmt.Fprintf(os.Stderr, "read %d comics\n", cnt)
 }
